11fb9b073aee6824e0e118cb5f09b5ca03e8bba6
Steve Lukis <stevelukis88@gmail.com>
2021-08-04 19:55:24 +0800

    Beautified add-book form.

--- src/add-book/components.js
+++ src/add-book/components.js
@@ -37,59 +37,59 @@ export function AddBook(props) {
         </div>
         <div className="card-body">
           <form onSubmit={handleSubmit}>
-            <div className="input">
-              <label className="input_label"
-                     htmlFor="inputBookTitle">
-                Title
-              </label>
+            <div className="form-floating">
               <input id="inputBookTitle"
-                     className="input_box"
+                     className="form-control"
                      type="text"
                      name="title"
                      value={book.title}
                      onChange={handleChange}
                      required/>
-            </div>
-            <div className="input">
-              <label htmlFor="inputBookAuthor"
-                     className="input_label">
-                Author
+              <label htmlFor="inputBookTitle">
+                Title
               </label>
+            </div>
+            <div className="form-floating mt-2">
               <input id="inputBookAuthor"
-                     className="input_box"
+                     className="form-control"
                      type="text"
                      name="author"
                      value={book.author}
                      onChange={handleChange}
                      required/>
-            </div>
-            <div className="input">
-              <label htmlFor="inputBookYear"
+              <label htmlFor="inputBookAuthor"
                      className="input_label">
-                Year
+                Author
               </label>
+            </div>
+            <div className="form-floating mt-2">
               <input id="inputBookYear"
-                     className="input_box"
+                     className="form-control"
                      type="number"
                      name="year"
                      value={book.year}
                      onChange={handleChange}
                      required/>
-            </div>
-            <div className="input">
-              <label htmlFor="inputBookIsComplete"
+              <label htmlFor="inputBookYear"
                      className="input_label">
-                Finished?
+                Year
               </label>
+            </div>
+            <div className="form-check mt-2">
               <input id="inputBookIsComplete"
-                     className="input_box"
+                     className="form-check-input"
                      name="finished"
                      checked={book.finished}
                      onChange={handleChange}
                      type="checkbox"/>
+              <label htmlFor="inputBookIsComplete"
+                     className="form-check-label">
+                Finished?
+              </label>
             </div>
-            <input type="submit"
-                   value="Submit"/>
+            <input className="form-control btn btn-primary mt-2"
+                   type="submit"
+                   value="Save"/>
           </form>
         </div>
       </div>
