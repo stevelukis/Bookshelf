d4290284dc201edf02dc58930ec3be40ec5d3386
Steve Lukis <stevelukis88@gmail.com>
2021-08-17 19:44:20 +0800

    Added bootstrap to the component book.

--- src/book/components.js
+++ src/book/components.js
@@ -2,21 +2,44 @@ export default function Book(props) {
   const {book} = props;
   const toogleButtonText = book.finished ? "Unfinish" : "Finish";
   return (
-    <div>
-      <div>
-        Title: {book.title}
+    <div className="container p-0">
+      <div className="row">
+        <div className="col-lg-3 col-sm-12">
+          Title:
+        </div>
+        <div className="col-lg-9 col-sm-12">
+          {book.title}
+        </div>
       </div>
-      <div>
-        Author: {book.author}
+      <div className="row">
+        <div className="col-lg-3 col-sm-12">
+          Author:
+        </div>
+        <div className="col-lg-9 col-sm-12">
+          {book.author}
+        </div>
       </div>
-      <div>
-        Year: {book.year}
+      <div className="row">
+        <div className="col-lg-3 col-sm-12">
+          Year:
+        </div>
+        <div className="col-lg-9 col-sm-12">
+          {book.year}
+        </div>
       </div>
-      <div>
-        <button onClick={() => props.handleToggleBook(book)}>{toogleButtonText}</button>
-      </div>
-      <div>
-        <button onClick={() => props.handleDeleteBook(book)}>Delete</button>
+      <div className="row mt-3">
+        <div className="col-lg-6 col-sm-12 p-1">
+          <button className="form-control btn btn-primary"
+                  onClick={() => props.handleToggleBook(book)}>
+            {toogleButtonText}
+          </button>
+        </div>
+        <div className="col-lg-6 col-sm-12 p-1">
+          <button className="form-control btn btn-danger"
+                  onClick={() => props.handleDeleteBook(book)}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   )
