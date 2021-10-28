b0612f1b4fb018034b76e3b4e43dd85c929e7f2c
Steve Lukis <stevelukis88@gmail.com>
2021-10-28 15:57:54 +0800

    Create model book

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class BookQs(models.QuerySet):
+    def finished(self):
+        return self.filter(finished=True)
+
+    def unfinished(self):
+        return self.filter(finished=False)
+
+
+class BookManager(models.Manager):
+    def get_queryset(self):
+        return BookQs(self.model, using=self._db)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=140)
+    author = models.CharField(max_length=140)
+    year = models.IntegerField()
+    finished = models.IntegerField()
+
+    objects = BookManager()
