20db9111e7f639fcfe6cdff7c581ed7474b429a5
Steve Lukis <stevelukis88@gmail.com>
2021-11-23 20:35:55 +0800

    Implement API in lookupDeleteBook

--- web/src/lookup.js
+++ web/src/lookup.js
@@ -2,18 +2,6 @@ const axios = require('axios')
 
 const BASE_URL = "http://127.0.0.1:8000/book/"
 
-const FINISHED_STORAGE_KEY = 'finished';
-const UNFINISHED_STORAGE_KEY = 'unfinished';
-
-function generateID() {
-  return Math.random().toString(36).substr(2, 5);
-}
-
-function getListFromWebStorage(listKey) {
-  const rawList = localStorage.getItem(listKey);
-  return JSON.parse(rawList)
-}
-
 async function getListFromAPI(endpoint) {
   return await axios.get(`${BASE_URL}${endpoint}`)
 }
@@ -34,17 +22,7 @@ export async function lookupSaveBook(book) {
 }
 
 export async function lookupDeleteBook(book) {
-  let list;
-  if (book.finished) {
-    list = await lookupFinishedList();
-  } else {
-    list = await lookupUnfinishedList();
-  }
-  list = list.filter(mBook => mBook.id !== book.id);
-  console.log(list)
-
-  const key = book.finished ? FINISHED_STORAGE_KEY : UNFINISHED_STORAGE_KEY;
-  localStorage.setItem(key, JSON.stringify(list))
+  await axios.delete(`${BASE_URL}${book.id}/`)
 }
 
 export async function lookupSetFinished(book, finished) {
