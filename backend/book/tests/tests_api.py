85da7bff17ebaaadc0b2870a5e1cd2d636662e05
Steve Lukis <stevelukis88@gmail.com>
2021-12-11 16:10:41 +0800

    Optimize tests

--- backend/book/tests/tests_api.py
+++ backend/book/tests/tests_api.py
@@ -8,11 +8,14 @@ from book.tests.base import BaseBookTest
 
 class BookAPITest(BaseBookTest):
     def verify_book(self, book: Book, book_dict: dict):
-        self.assertEqual(book.id, book_dict['id'])
         self.assertEqual(book.title, book_dict['title'])
         self.assertEqual(book.author, book_dict['author'])
         self.assertEqual(book.year, book_dict['year'])
         self.assertEqual(book.finished, book_dict['finished'])
+
+    def verify_book_with_id(self, book: Book, book_dict: dict):
+        self.assertEqual(book.id, book_dict['id'])
+        self.verify_book(book, book_dict)
 
     def test_create(self):
         for i in range(5):
@@ -68,32 +71,23 @@ class BookAPITest(BaseBookTest):
 
             self.verify_book(updated_book, book_dict)
 
+    def _test_set_finished(self, book_list: list[Book], finished: bool):
+        for book in book_list:
+            post_data = {
+                'finished': finished
+            }
+            url = reverse('book-detail', kwargs={'pk': book.id})
+            response = self.client.patch(url, post_data, content_type='application/json')
+            self.assertEqual(response.status_code, 200)
+
+            book_dict = response.data
+            updated_book = Book.objects.get(id=book.id)
+
+            self.verify_book_with_id(updated_book, book_dict)
+
     def test_set_finished(self):
-        for book in self.book_finished_list:
-            post_data = {
-                'finished': False
-            }
-            url = reverse('book-detail', kwargs={'pk': book.id})
-            response = self.client.patch(url, post_data, content_type='application/json')
-            self.assertEqual(response.status_code, 200)
-
-            book_dict = response.data
-            updated_book = Book.objects.get(id=book_dict['id'])
-
-            self.verify_book(updated_book, book_dict)
-
-        for book in self.book_unfinished_list:
-            post_data = {
-                'finished': True
-            }
-            url = reverse('book-detail', kwargs={'pk': book.id})
-            response = self.client.patch(url, post_data, content_type='application/json')
-            self.assertEqual(response.status_code, 200)
-
-            book_dict = response.data
-            updated_book = Book.objects.get(id=book_dict['id'])
-
-            self.verify_book(updated_book, book_dict)
+        self._test_set_finished(self.book_finished_list, True)
+        self._test_set_finished(self.book_unfinished_list, False)
 
     def test_delete(self):
         book_list = self.book_finished_list + self.book_unfinished_list
@@ -105,32 +99,27 @@ class BookAPITest(BaseBookTest):
             deleted_book = Book.objects.filter(id=book.id).first()
             self.assertIsNone(deleted_book)
 
+    def _test_list_finished(self, finished: bool):
+        if finished:
+            url = reverse('book-finished')
+        else:
+            url = reverse('book-unfinished')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        book_list = Book.objects.filter(finished=finished)
+        book_result = response.data
+
+        self.assertNotEqual(len(book_list), 0)
+        self.assertEqual(len(book_list), len(book_result))
+
+        for i in range(len(book_list)):
+            book = book_list[i]
+            book_dict = book_result[i]
+            self.verify_book_with_id(book, book_dict)
+
     def test_list_finished(self):
-        url = reverse('book-finished')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self._test_list_finished(True)
+        self._test_list_finished(False)
 
-        book_list = Book.objects.filter(finished=True)
-        book_result = response.data
-
-        self.assertEqual(len(book_list), len(book_result))
-
-        for i in range(len(book_list)):
-            book = book_list[i]
-            book_dict = book_result[i]
-            self.verify_book(book, book_dict)
-
-    def test_list_unfinished(self):
-        url = reverse('book-unfinished')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        book_list = Book.objects.filter(finished=False)
-        book_result = response.data
-
-        self.assertEqual(len(book_list), len(book_result))
-
-        for i in range(len(book_list)):
-            book = book_list[i]
-            book_dict = book_result[i]
-            self.verify_book(book, book_dict)
