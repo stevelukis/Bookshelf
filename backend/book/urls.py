180b86ed890c620e675a8238fd20a3dadcb21c8f
Vikas Donta <78430602+Vikasdonta1@users.noreply.github.com>
2023-04-29 20:54:23 +0530

    main

--- backend/book/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from book import views
-
-router = routers.DefaultRouter()
-router.register('', views.BookViewSet, basename='book')
-urlpatterns = router.urls
-print(urlpatterns)
